a4aa7dee3d020b808526d28fd9bf1d201b513737
Chris Frater <chrisfrater@google.com>
2018-07-23 12:18:10 -0700

    cleaned up audioProfile, linting

--- samples/system-test/quickstart.test.js
+++ samples/system-test/quickstart.test.js
@@ -12,8 +12,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 /* eslint-disable */
-'use strict';
+
+ 'use strict';
 
 const fs = require(`fs`);
 const path = require(`path`);
