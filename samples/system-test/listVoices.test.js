da0d08bb42c6b4d1983afdb0b40cdf46e294aa75
Chris Frater <chrisfrater@google.com>
2018-07-23 11:53:25 -0700

    linter quick fix

--- samples/system-test/listVoices.test.js
+++ samples/system-test/listVoices.test.js
@@ -12,7 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+/* eslint-disable */
 'use strict';
 
 const path = require(`path`);
